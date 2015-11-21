35b2a440c7d636dfbbff51beb836becad4651912
Rodrigo Queipo <rodrigoq@gmail.com>
2015-11-20 23:24:13 -0300

    Good disposing of objects in Graph. Code formatting.

--- GeneticAssigner/Graph.cs
+++ GeneticAssigner/Graph.cs
@@ -23,57 +23,57 @@ using GeneticAlgorithm;
 
 namespace GeneticAssigner
 {
-    public class Graph : Control
-    {
-        public Graph()
-        {
+	public class Graph : Control
+	{
+		public Graph()
+		{
 			//this.Height = 200;
 			//this.Width = 500;
 			BackColor = Color.White;
-        }
+		}
 
-        public void DrawFitness(int generation, double fitness)
-        {
-            //Pen myPen = new Pen(Color.FromArgb(r.Next(0, 255), r.Next(0, 255), r.Next(0, 255)));//Color.Blue);
+		public void DrawFitness(int generation, double fitness)
+		{
+			//Pen myPen = new Pen(Color.FromArgb(r.Next(0, 255), r.Next(0, 255), r.Next(0, 255)));//Color.Blue);
 
-            double fitmin = 25.8;
-            double fitmax = 26.0;
+			double fitmin = 25.8;
+			double fitmax = 26.0;
 
-            double y = ((double)Height * (fitness - fitmin) / (fitmax - fitmin)) - fitmin;
-            int x = Width * generation / 2000;
-            Pen pen = new Pen(Color.Blue);
+			double y = Height * (fitness - fitmin) / (fitmax - fitmin) - fitmin;
+			int x = Width * generation / 2000;
+			using (Pen pen = new Pen(Color.Blue))
+			{
+				using (Graphics graphics = CreateGraphics())
+				{
+					graphics.DrawRectangle(pen, x, (float)(Height - y), 1.0f, 1.0f);
+				}
+			}
 
-            Graphics graphics = CreateGraphics();
-            graphics.DrawRectangle(pen, (float)x, (float)((double)Height - y), 1.0f, 1.0f);
-            pen.Dispose();
-            graphics.Dispose();
+		}
 
-        }
+		public void DrawStudents(int generation, IIndividual individual)
+		{
+			//Pen myPen = new Pen(Color.FromArgb(r.Next(0, 255), r.Next(0, 255), r.Next(0, 255)));//Color.Blue);
 
-        public void DrawStudents(int generation, IIndividual individual)
-        {
-            //Pen myPen = new Pen(Color.FromArgb(r.Next(0, 255), r.Next(0, 255), r.Next(0, 255)));//Color.Blue);
+			using (Graphics graphics = CreateGraphics())
+			{
+				int y = Height * individual.Assigned / individual.Students.Count;
+				int x = Width * generation / 2000;
+				using (Pen pen = new Pen(Color.Red))
+				{
+					graphics.DrawRectangle(pen, x, Height - y, 1, 1);
+				}
 
-            int y = Height * individual.Assigned / individual.Students.Count;
-            int x = Width * generation / 2000;
-            Pen pen = new Pen(Color.Red);
+				int y1 = Height * individual.NotAssigned / individual.Students.Count;
+				int x1 = Width * generation / 2000;
 
-            Graphics graphics = CreateGraphics();
-            graphics.DrawRectangle(pen, x, Height - y, 1, 1);
-            pen.Dispose();
+				using (Pen pen1 = new Pen(Color.Brown))
+				{
+					graphics.DrawRectangle(pen1, x1, Height - y1, 1, 1);
+				}
+			}
 
-            int y1 = Height * individual.NotAssigned / individual.Students.Count;
-            int x1 = Width * generation / 2000;
-            Pen pen1 = new Pen(Color.Brown);
+		}
 
-            graphics.DrawRectangle(pen1, x1, Height - y1, 1, 1);
-
-
-            pen1.Dispose();
-
-            graphics.Dispose();
-
-        }
-
-    }
+	}
 }
